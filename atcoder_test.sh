b9061259516c5f146e07f0476dd715d67ce0a822
AnDeriens <katsuya.ando496@gmail.com>
2020-10-13 02:12:48 +0900

    正解数と間違いの数を表示

--- atcoder_test.sh
+++ atcoder_test.sh
@@ -7,15 +7,26 @@ inputdir="$testdir/in"
 outputdir="$testdir/out"
 inputfiles=$(ls $inputdir)
 
+noProblem=0
+noCorrect=0
+noWrong=0
+
 for testfilename in $inputfiles; do
     output=$(python3 $executefile < "$testdir/in/$testfilename")
     expected=$(cat "$outputdir/$testfilename")
     # echo $output
     # echo $expected
+    noProblem=$((++noProblem))
     if [ "$output" == "$expected" ]; then
         echo "True: $testfilename"
+        noCorrect=$((++noCorrect))
     else
         echo "False: $testfilename"
+        noWrong=$((++noWrong))
     fi
 done
+
+echo "テスト数: $noProblem"
+echo "正解: $noCorrect"
+echo "間違い: $noWrong"
 
